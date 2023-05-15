c04e444e7d9e797e016d2e9c787a4c1b276c7304
GreenlightShadow <artyom199levchenko@gmail.com>
2023-05-15 03:30:51 +0300

    [14.05.23] homework_1 rework + homework_2

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ class App extends React.Component {
     regexps = {
         firstName: "^[A-Z]{1}[a-z]+?$",
         lastName: "^[A-Z]{1}[a-z]+?$",
-        birthday: "^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$",
-        phone: "^[0-9]{1}\-[0-9]{4}\-[0-9]{2}\-[0-9]{2}$",
-        website: "^((https):\/\/)"
+        birthday: "^[0-9]{4}-[0-9]{2}-[0-9]{2}$",
+        phone: "^[0-9]{1}-[0-9]{4}-[0-9]{2}-[0-9]{2}$",
+        website: "^((https)://)"
     }
 
     constructor(props) {
